dab00cdd2113dc70d18d37ab944bfbdccc1d5ea3
HC Zhang <huacheng.zhang@gmail.com>
2018-08-17 10:15:33 +0900

    Use connect on square

--- src/components/board.js
+++ src/components/board.js
@@ -1,28 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BLUE_PRINT, CTRL_KEYS } from "./const";
-import { ActionCreators } from "../actions";
+import { Square } from "./square";
 import { connect } from "react-redux";
-
-class Square extends React.Component {
-  render() {
-    let squareClass = "square";
-    squareClass += this.props.active ? " active" : "";
-    squareClass += this.props.solved ? " solved" : "";
-
-    return (
-      <button
-        className={squareClass}
-        position={this.props.position}
-        onClick={this.props.onClick}
-        onKeyDown={this.props.onKeyDown}
-        onKeyPress={this.props.onKeyPress}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
-}
 
 class BoardComp extends React.Component {
   constructor(props) {
@@ -30,45 +9,8 @@ class BoardComp extends React.Component {
     this.comps = Array(100);
   }
 
-  renderSquare(i) {
-    const isActive = this.props.state.active === i;
-    const isSolved = this.props.state.squares[i] === BLUE_PRINT[i];
-    return (
-      <Square
-        ref={comp => (this.comps[i] = comp)}
-        key={i}
-        position={i}
-        active={isActive}
-        solved={isSolved}
-        value={this.props.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-        onKeyDown={e => this.handleKeyDown(e)}
-        onKeyPress={e => this.handleKeyPress(e)}
-      />
-    );
-  }
-
   componentDidUpdate() {
     ReactDOM.findDOMNode(this.comps[this.props.state.active]).focus();
-  }
-
-  handleClick(i) {
-    this.props.handler(ActionCreators.activate(i));
-  }
-
-  /** Keyboard control keys */
-  handleKeyDown(e) {
-    let cmd = CTRL_KEYS[e.keyCode];
-    if (!cmd) return;
-    e.preventDefault();
-    this.props.handler(ActionCreators.control(cmd));
-  }
-
-  /** Keyboard press except control keys */
-  handleKeyPress(e) {
-    if (e.charCode >= 48 && e.charCode <= 57) {
-      this.props.handler(ActionCreators.control(e.charCode - 48));
-    }
   }
 
   render() {
@@ -76,7 +18,10 @@ class BoardComp extends React.Component {
       <div>
         {"0123456789".split("").map(row => (
           <div key={"row" + row} className="board-row">
-            {"0123456789".split("").map(col => this.renderSquare(+row * 10 + +col))}
+            {"0123456789".split("").map(col => {
+              let i = +row * 10 + +col;
+              return <Square ref={comp => (this.comps[i] = comp)} key={i} position={i} />;
+            })}
           </div>
         ))}
       </div>
@@ -85,7 +30,7 @@ class BoardComp extends React.Component {
 }
 
 const mapStateToProps = state => ({ state: state.board.present });
-const mapDispatchToProps = dispatch => ({ handler: action => dispatch(action) });
+const mapDispatchToProps = dispatch => ({});
 
 export const Board = connect(
   mapStateToProps,
---
+++ src/components/square.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { BLUE_PRINT, CTRL_KEYS } from "./const";
+import { ActionCreators } from "../actions";
+import { connect } from "react-redux";
+
+class SquareComp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  /** Mouse click changes square focus. */
+  handleClick() {
+    this.props.handler(ActionCreators.activate(this.props.position));
+  }
+
+  /** Keyboard control keys */
+  handleKeyDown(e) {
+    let cmd = CTRL_KEYS[e.keyCode];
+    if (!cmd) return;
+    e.preventDefault();
+    this.props.handler(ActionCreators.control(cmd));
+  }
+
+  /** Keyboard press except control keys */
+  handleKeyPress(e) {
+    if (e.charCode >= 48 && e.charCode <= 57) {
+      this.props.handler(ActionCreators.control(e.charCode - 48));
+    }
+  }
+
+  render() {
+    let position = this.props.position;
+    let value = this.props.state.squares[position];
+    let isActive = this.props.position === this.props.state.active;
+    let isSolved = BLUE_PRINT[position] === value;
+
+    let squareClass = "square";
+    squareClass += isActive ? " active" : "";
+    squareClass += isSolved ? " solved" : "";
+
+    return (
+      <button
+        className={squareClass}
+        onClick={this.handleClick}
+        onKeyDown={this.handleKeyDown}
+        onKeyPress={this.handleKeyPress}
+      >
+        {value}
+      </button>
+    );
+  }
+}
+
+const mapStateToProps = state => ({ state: state.board.present });
+const mapDispatchToProps = dispatch => ({ handler: action => dispatch(action) });
+
+export const Square = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SquareComp);
